1c4971f57ab675f366c290dba6cbc2d517322d61
eclipse_user <eclipse_user@lss.com>
2020-01-09 17:22:41 +0800

    jjjjj

--- src/main/java/com/lss/cn/Happy.java
+++ src/main/java/com/lss/cn/Happy.java
@@ -4,7 +4,8 @@ public class Happy {
 
 	public static void main(String[] args) {
 		System.out.println("master 001");
-		System.out.println("dev01  001 GitTest02");
+		System.out.println("dev01  001 GitTest"
+				+ "0000");
 	}
 
 }
