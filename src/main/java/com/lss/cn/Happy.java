b7024cdad269ae712c92953ddf8c60eab1739efe
tom_glb <goodMorning_glb@qq.com>
2020-01-09 17:05:48 +0800

    dev02 002

--- src/main/java/com/lss/cn/Happy.java
+++ src/main/java/com/lss/cn/Happy.java
@@ -4,7 +4,7 @@ public class Happy {
 
 	public static void main(String[] args) {
 		System.out.println("master 001");
-		System.out.println("dev01 001");
+		System.out.println("dev02 002");
 	}
 
 }
